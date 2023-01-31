afd3429d7b5ff13ee199517b0b5f515cc85ac916
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2023-01-30 23:23:15 -0500

    Added more events: entityDamaged(), bulletFired()

--- lua/entities/gmod_wire_expression2/core/custom/events.lua
+++ lua/entities/gmod_wire_expression2/core/custom/events.lua
@@ -3,19 +3,25 @@
 
 E2Lib.RegisterExtension("extendedfunc", false, "Extended e2 functions and events")
 
+local extendedfunc = {}
+extendedfunc.gravholding = {}
+extendedfunc.handholding = {}
+
 E2Lib.registerEvent("entityCollide", {"t"})
 E2Lib.registerEvent("gravGunPunt", {"e","e"})
 E2Lib.registerEvent("gravGunPickup", {"e","e"})
 E2Lib.registerEvent("physicsPickup", {"e","e"})
 E2Lib.registerEvent("physicsDropped", {"e","e"})
 E2Lib.registerEvent("physicsThrown", {"e","e"})
+E2Lib.registerEvent("entityDamaged", {"e","t"})
+E2Lib.registerEvent("bulletFired", {"e","t"})
 
 if SERVER then
 	util.AddNetworkString("e2_propresize")
 end
 
 local function collisionCallback(ent, data)
-	local e2data = {
+	local coldata = {
 		s = { 
 			OurEntity = ent,
 			HitPos = data.HitPos,
@@ -49,7 +55,7 @@ local function collisionCallback(ent, data)
 		n = {}, ntypes = {},
 		size = 11
 	}
-	E2Lib.triggerEvent("entityCollide", {e2data})
+	E2Lib.triggerEvent("entityCollide", {coldata})
 end
 
 __e2setcost(2)
@@ -64,29 +70,78 @@ end
 e2function void entity:removeCollisionCallback()
 
 	if !IsValid(this) then self:throw("Invalid entity!", "") return end
-	if not this.e2CollisionCallback then self:throw("Entity already had its collision callback removed!", "") return end
+	if !this.e2CollisionCallback then self:throw("Entity already had its collision callback removed!", "") return end
 	this:RemoveCallback("PhysicsCollide", this.e2CollisionCallback)
 
 end
 
 hook.Add("GravGunOnPickedUp", "extendedcore_gravpickup", function(ply,ent)
 	E2Lib.triggerEvent("gravGunPickup", {ply, ent})
+	extendedfunc.gravholding[ent] = true
 end)
 
 hook.Add("GravGunPunt", "extendedcore_gravpunt", function(ply, ent)
 	timer.Simple(0, function() E2Lib.triggerEvent("gravGunPunt", {ply, ent}) end)
+	extendedfunc.gravholding[ent] = false
 end)
 
 hook.Add("OnPlayerPhysicsPickup", "extendedcore_physpickup", function(ply, ent)
 	E2Lib.triggerEvent("physicsPickup", {ply, ent})
+	extendedfunc.handholding[ent] = true
 end)
 
 hook.Add("OnPlayerPhysicsDrop", "extendedcore_physdrop", function(ply, ent, thrown)
-	if thrown then
-		timer.Simple(0, function() E2Lib.triggerEvent("physicsThrown", {ply, ent}) end)
-	else
-		timer.Simple(0, function() E2Lib.triggerEvent("physicsDropped", {ply, ent}) end)
-	end
+	-- this hook is run before the player actually dropped the item, for entity manipulation
+	timer.Simple(0, function() E2Lib.triggerEvent("physics"..(thrown and "Thrown" or "Dropped"), {ply, ent}) end)
+	extendedfunc.handholding[ent] = false
+end)
+
+hook.Add("EntityTakeDamage", "extendedcore_entdamage", function(ent, dmginfo)
+	local dmgdata = {
+		s = { 
+			Inflictor = dmginfo:GetInflictor(),
+			Attacker = dmginfo:GetAttacker(),
+			DamagePos = dmginfo:GetDamagePosition(),
+			IsBulletDamage = dmginfo:IsBulletDamage() and 1 or 0,
+			IsExplosionDamage = dmginfo:IsExplosionDamage() and 1 or 0,
+			IsFallDamage = dmginfo:IsFallDamage() and 1 or 0,
+			Damage = dmginfo:GetDamage(),
+		},
+		stypes = {
+			Inflictor = "e",
+			Attacker = "e",
+			DamagePos = "v",
+			IsBulletDamage = "n",
+			IsExplosionDamage = "n",
+			IsFallDamage = "n",
+			Damage = "n",
+		},
+		n = {}, ntypes = {},
+		size = 7
+	}
+	-- timer is here to fix infinite loop crash if the entity damages itself.
+	timer.Simple(0, function() E2Lib.triggerEvent("entityDamaged", {ent, dmgdata}) end)
+end)
+
+hook.Add("EntityFireBullets", "extendedfunc_firebullets", function(ent, bulletinfo)
+	local bulletdata = {
+		s = {
+			Direction = bulletinfo.Dir,
+			Origin = bulletinfo.Src,
+			BulletCount = bulletinfo.Num,
+			AmmoType = bulletinfo.AmmoType,
+		},
+		stypes = {
+			Direction = "v",
+			Origin = "v",
+			BulletCount = "n",
+			AmmoType = "s",
+		},
+		n = {}, ntypes = {},
+		size = 4
+	}
+	-- timer is here to fix infinite loop crash if you fire a bullet inside of the event.
+	timer.Simple(0, function() E2Lib.triggerEvent("bulletFired", {ent, bulletdata}) end)
 end)
 
 e2function angle vector:axisToAng()
@@ -95,6 +150,16 @@ end
 
 e2function vector angle:angToAxis()
 	return Vector(this[2],this[1],this[3])
+end
+
+e2function number entity:isPlayerHoldingGrav()
+	print(extendedfunc.gravholding[this])
+	return extendedfunc.gravholding[this] and 1 or 0
+end
+
+e2function number entity:isPlayerHoldingHand()
+	print(extendedfunc.gravholding[this])
+	return extendedfunc.handholding[this] and 1 or 0
 end
 
 local function ResizePhysics( ent, scale )
@@ -117,21 +182,22 @@ local function ResizePhysics( ent, scale )
 		end
 
 	end
-	ent:PhysicsDestroy()
 	ent:PhysicsInit( SOLID_VPHYSICS )
 	ent:PhysicsInitMultiConvex( physmesh )
 	ent:EnableCustomCollisions( true )
-	return
+	return true
 
 end
 
-e2function void entity:scaleEnt(vector scale)
+e2function vector entity:getScale()
+	return this.e2_scale or Vector(1,1,1)
+end
 
+e2function void entity:scaleEnt(vector scale)
 	if !self.player:IsUserGroup("superadmin") then --change this to whatever group you want if you're on a server
 		self:throw("Function is superadmin only!", "")
 		return
 	end
-
 	if IsValid(this) then
 		local phys = this:GetPhysicsObject()
 
@@ -140,7 +206,7 @@ e2function void entity:scaleEnt(vector scale)
 			return
 		end
 
-		local vel = this:GetVelocity()
+		local vel = phys:GetVelocity()
 		local angvel = phys:GetAngleVelocity()
 		local pos = this:GetPos()
 		local mass = phys:GetMass()
@@ -150,7 +216,7 @@ e2function void entity:scaleEnt(vector scale)
 		scale[2] = math.Clamp(scale[2],-20,20)
 		scale[3] = math.Clamp(scale[3],-20,20)
 		if ResizePhysics(this, scale) then
-			net.Start("set_visual_size")
+			net.Start("e2_propresize")
 				net.WriteEntity(this)
 				net.WriteFloat(scale[1])
 				net.WriteFloat(scale[2])
@@ -163,6 +229,7 @@ e2function void entity:scaleEnt(vector scale)
 			phys:SetAngleVelocity(angvel)
 			phys:EnableMotion(frozen)
 			phys:SetMaterial(phys_mat)
+			this.e2_scale = scale
 		end
 	else
 		self:throw("Invalid entity!", "") 
@@ -184,20 +251,22 @@ e2function void entity:resetScale()
 		local mass = phys:GetMass()
 		local frozen = phys:IsMotionEnabled()
 		local phys_mat = phys:GetMaterial()
-		ResizePhysics(this, scale)
-		net.Start("e2_propresize")
-			net.WriteEntity(this)
-			net.WriteFloat(1)
-			net.WriteFloat(1)
-			net.WriteFloat(1)
-		net.Broadcast()
-		this:SetPos(pos)
-		phys = this:GetPhysicsObject()
-		phys:SetMass(mass * scale:Length())
-		phys:SetVelocity(vel)
-		phys:SetAngleVelocity(angvel)
-		phys:EnableMotion(frozen)
-		phys:SetMaterial(phys_mat)
+		if ResizePhysics(this, scale) then
+			net.Start("e2_propresize")
+				net.WriteEntity(this)
+				net.WriteFloat(1)
+				net.WriteFloat(1)
+				net.WriteFloat(1)
+			net.Broadcast()
+			this:SetPos(pos)
+			phys = this:GetPhysicsObject()
+			phys:SetMass(mass * scale:Length())
+			phys:SetVelocity(vel)
+			phys:SetAngleVelocity(angvel)
+			phys:EnableMotion(frozen)
+			phys:SetMaterial(phys_mat)
+			this.e2_scale = scale
+		end
 	else
 		self:throw("Invalid entity!", "") 
 	end
